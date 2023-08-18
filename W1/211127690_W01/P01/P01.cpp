811ccb0cb58b1d0a6c55f6e81cf3688f9dfef10b
wwebb <21127690@student.hcmus.edu.vn>
2023-08-18 12:53:18 +0700

    delete W1

--- W1/211127690_W01/P01/P01.cpp
+++
@@ -1,6 +0,0 @@
-#include<iostream>
-using namespace std;
-int main() {
-	cout << "P01" << endl;
-	return 0;
-}
--- W1/211127690_W01/P02/P02.cpp
+++
@@ -1,6 +0,0 @@
-#include<iostream>
-using namespace std;
-int main() {
-	cout << "P02" << endl;
-	return 0;
-}
