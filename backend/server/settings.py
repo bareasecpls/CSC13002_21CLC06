fd919a8abf242ad5c25895ffe9dbd9a23ea25bf8
bareasecpls <21127690@student.hcmus.edu.vn>
2023-09-04 22:01:19 +0700

    backend: add db

--- backend/server/settings.py
+++ backend/server/settings.py
@@ -27,11 +27,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost',
-    'http://127.0.0.1',
-    'http://0.0.0.0',
-]
+CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOWED_ORIGINS = [
+#     'http://localhost',
+#     'http://127.0.0.1',
+#     'http://0.0.0.0',
+# ]
 
 CORS_ALLOW_CREDENTIALS = True
 
