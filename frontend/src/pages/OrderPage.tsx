4f3e0d995d25a25e3dffa1921d6950b8319b2fc3
bareasecpls <21127690@student.hcmus.edu.vn>
2023-09-07 23:28:57 +0700

    frontend: add checkout order page

--- frontend/src/pages/OrderPage.tsx
+++ frontend/src/pages/OrderPage.tsx
@@ -1,85 +1,134 @@
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import { useAuthContext } from "@/contexts/AuthContext";
+import { useToast } from "@/components/ui/use-toast";
+import { ToastAction } from "@/components/ui/toast";
+import { useNavigate } from "react-router-dom";
+import { Button } from "@/components/ui/button";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
-export default function OrderPage() {
+export default function CartPage() {
   const navigate = useNavigate();
+  const { toast } = useToast();
+  const { authContext } = useAuthContext();
   const [errorMessage, setErrorMessage] = useState("");
-  const { authContext, setAuthContext } = useAuthContext();
+
+  useEffect(() => {
+    if (!authContext.isAuthenticated) {
+      navigate("/");
+    }
+  }, [authContext]);
 
   const submitHandler = (e: any) => {
     e.preventDefault();
-    console.log("submitHandler");
-    if (authContext.isAuthenticated) {
-      setErrorMessage("User is already logged in!");
-      return;
-    }
-    const email = e.currentTarget.querySelector("#email").value.trim();
-    const password = e.currentTarget.querySelector("#password").value.trim();
-    if (!email) {
-      setErrorMessage("Email can not contain white spaces!");
-      return;
-    }
-    if (!password) {
-      setErrorMessage("Password can not contain white spaces!");
-      return;
-    }
-    handleLogin(email, password);
+    const target = e.currentTarget;
+    console.log(target);
+    // const email = target.querySelector("#email").value.trim();
+    const fullname = target.querySelector("#fullname")?.value?.trim();
+    const phone = target.querySelector("#phone")?.value?.trim();
+    const shipping_method = target.querySelector("#shipping")?.value?.trim();
+    const payment_method = target.querySelector("#payment")?.value?.trim();
+
+    handleCheckout({
+      fullname: fullname,
+      // email: email,
+      phone: phone,
+      shipping_method: shipping_method || "COD",
+      payment_method: payment_method || "CASH",
+    });
   };
 
-  const handleLogin = async (email: string, password: string) => {
+  const handleCheckout = async (info: any) => {
     try {
-      const { data } = await axios.post("/api/login/", {
-        email: email,
-        password: password,
+      await axios.post("/api/orders/" + authContext.user.id + "/create", info);
+      toast({
+        title: "Created an order successfully!",
+        action: <ToastAction altText="ok">OK</ToastAction>,
       });
-      setAuthContext({ ...authContext, user: data, isAuthenticated: true });
       navigate("/");
     } catch (err: any) {
       console.log(err.response.data);
       const message =
-        err.response.data.error ||
-        err.response.data.email[0] ||
-        err.response.data.password[0];
+        err.response.data.error || "Can not create order!"
       setErrorMessage(message);
     }
   };
 
   return (
-    <div className="mt-12">
-      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto lg:py-0">
-        <div className="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
+    <>
+      <div className="mx-auto max-w-screen-xl my-8">
+        <div className="w-full bg-white xl:p-0">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
-              Log in to your account
+            <h1 className="text-2xl font-semibold leading-tight tracking-tight text-gray-900 md:text-3xl">
+              Delivery Information
             </h1>
             <form className="space-y-4 md:space-y-6" onSubmit={submitHandler}>
               <div>
                 <label className="block mb-2 text-sm font-medium text-gray-900">
-                  Your email
+                  Full name
+                </label>
+                <Input id="fullname" placeholder="First and last name" />
+              </div>
+              <div>
+                <label className="block mb-2 text-sm font-medium text-gray-900">
+                  Email
                 </label>
                 <Input
                   type="email"
-                  name="email"
                   id="email"
                   placeholder="name@domain.com"
-                  required
                 />
               </div>
               <div>
                 <label className="block mb-2 text-sm font-medium text-gray-900">
-                  Password
+                  Phone number
                 </label>
-                <Input
-                  type="password"
-                  name="password"
-                  id="password"
-                  placeholder="••••••••"
-                  required
-                />
+                <Input id="phone" placeholder="0123456789" />
+              </div>
+              <div>
+                <label className="block mb-2 text-sm font-medium text-gray-900">
+                  Shipping method
+                </label>
+                <Select>
+                  <SelectTrigger className="w-[180px]">
+                    <SelectValue id="shipping" placeholder="Shipping method" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectGroup>
+                      <SelectLabel>Shipping method</SelectLabel>
+                      <SelectItem value="COD">COD</SelectItem>
+                    </SelectGroup>
+                  </SelectContent>
+                </Select>
+              </div>
+              <div>
+                <label className="block mb-2 text-sm font-medium text-gray-900">
+                  Payment method
+                </label>
+                <Select>
+                  <SelectTrigger className="w-[180px]">
+                    <SelectValue id="payment" placeholder="Payment method" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectGroup>
+                      <SelectLabel>Payment method</SelectLabel>
+                      <SelectItem value="CASH">Cash</SelectItem>
+                      <SelectItem value="BANK">Banking</SelectItem>
+                      <SelectItem value="CREDIT">Credit card</SelectItem>
+                    </SelectGroup>
+                  </SelectContent>
+                </Select>
               </div>
               {errorMessage ? (
                 <div
@@ -89,42 +138,23 @@ export default function OrderPage() {
                   {errorMessage}
                 </div>
               ) : null}
-              <div className="flex items-center justify-between">
-                <div className="flex items-start">
-                  <div className="flex items-center h-5">
-                    <Input
-                      id="remember"
-                      aria-describedby="remember"
-                      type="checkbox"
-                    />
-                  </div>
-                  <div className="ml-3 text-sm">
-                    <label className="text-gray-500">Remember me</label>
-                  </div>
-                </div>
-                <a
-                  href="#"
-                  className="text-sm font-medium text-primary-600 hover:underline"
+              <div className="flex justify-between mt-4">
+                <Button
+                  className="text-base py-6"
+                  variant={"outline"}
+                  onClick={() => navigate("/cart")}
                 >
-                  Forgot password?
-                </a>
+                  <FontAwesomeIcon icon={faChevronLeft} />
+                  <span className="ml-4">Back to Cart</span>
+                </Button>
+                <Button type="submit" className="text-base py-6 px-8">
+                  Checkout
+                </Button>
               </div>
-              <Button type="submit" className="w-full">
-                Sign in
-              </Button>
-              <p className="text-sm font-light text-gray-500">
-                Not registered?{" "}
-                <a
-                  href="/register"
-                  className="font-medium text-primary-600 hover:underline"
-                >
-                  Create account
-                </a>
-              </p>
             </form>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
